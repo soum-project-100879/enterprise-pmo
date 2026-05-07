427400e425ab52fcef6ec32ac5774135e025cc1e
soum-project-100879 <soum.projectleader@gmail.com>
2026-05-07 15:24:14 +0530

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{padding:'40px', fontFamily:'Arial'}}>
+      <h1>Enterprise PMO</h1>
+      <p>Deployment-ready React application.</p>
+    </div>
+  )
+}
